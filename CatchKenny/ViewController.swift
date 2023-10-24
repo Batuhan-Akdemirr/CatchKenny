733799eead9aa0b04130c27d8628d2f6c0540d8e
Batuhan Akdemir <batuhan_Akdemir05@hotmail.com>
2023-10-24 14:27:14 +0300

    project design completed

--- CatchKenny/ViewController.swift
+++ CatchKenny/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeController.swift
 //  CatchKenny
 //
 //  Created by Batuhan Akdemir on 24.10.2023.
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+   
 
 }
-
